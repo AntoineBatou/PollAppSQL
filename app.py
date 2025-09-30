832b0ae884c367171268e617fea339f4a675da3c
Antoine Batou <toubas.antoine@gmail.com>
2025-09-30 11:14:48 +0200

    Ajout pourcentage résultats

--- app.py
+++ app.py
@@ -1,8 +1,89 @@
 import os
 import psycopg2
 from psycopg2.errors import DivisionByZero
+from psycopg2.extras import execute_values
 from dotenv import load_dotenv
-import database
+import data
+
+DATABASE_PROMPT = "Saisissez la valeur DATABASE_URI ou laissez-la vide pour charger le fichier .env : "
+MENU_PROMPT = """-- Menu --
+1) Créer un nouveau sondage
+2) Lister les sondages ouverts
+3) Voter sur un sondage
+4) Afficher les votes du sondage
+5) Sélectionner un gagnant au hasard à partir d'une option du sondage
+6) Quitter
+Saisir votre choix : """
+NEW_OPTION_PROMPT = "Saisissez le texte de la nouvelle option (ou laissez-le vide pour ne plus ajouter d'options) : "
+
+def prompt_create_poll(connex):
+    poll_title = input("Veuillez entrer un titre pour le sondage : ")
+    poll_owner = input("QUel propriétaire du sondage ? ")
+    options = []
+    while new_option := input(NEW_OPTION_PROMPT):
+        options.append(new_option)
+    data.create_poll(connex, poll_title, poll_owner, options)
+
+def list_open_polls(connex):
+    polls = data.get_polls(connex)
+
+    for _id, title, owner in polls:
+        print(f"{_id}, - {title}, crée par {owner}.")
+
+def prompt_vote_poll(connex):
+    poll_id = input("Pour quel sondage voulez-vous voter (id) ? ")
+    poll_options = data.get_polls_details(connex, poll_id)
+    _print_poll_options(connex, poll_options)
+    option_id = input("Pour quelle option voulez-vous voter ?")
+    username = input("Quel utilisateur êtes-vous ?")
+    data.add_poll_vote(connex, username, option_id)
+
+def _print_poll_options(connex, poll_with_options):
+    for option in poll_with_options:
+        print(f"{option[3]}: {option[4]}")
+
+def show_poll_vote(connex):
+    list_open_polls(connex)
+    poll_id = input("Pour quel sondage?")
+    try:
+        poll_and_votes = data.get_poll_and_vote_results(connex, poll_id)
+    except DivisionByZero:
+        print("Pas de vote pour ce sondage !")
+    else:
+        for _id, option_text, count, percentage in poll_and_votes:
+            print(f"{option_text} a obtenu {count} votes ({percentage:.2f}% du total)")
+
+def randomize_poll_winner(connex):
+    list_open_polls(connex)
+    poll_id = input("Séléctionnez un sondage pour lequel vous souhaitez désigner un gagnant !")
+    poll_with_options = data.get_polls_details(connex, poll_id)
+    _print_poll_options(connex, poll_with_options)
+    option_id = input("Séléctionnez l'ID de la réponse gagnante, nous séléctionnerons un gagant au hasard !")
+    winner = data.get_random_poll_vote(connex, option_id)
+    print(f"Le gagnant est {winner[0]}")
 
 
+MENU_OPTIONS = {
+    "1": prompt_create_poll,
+    "2": list_open_polls,
+    "3": prompt_vote_poll,
+    "4": show_poll_vote,
+    "5": randomize_poll_winner
+}
 
+def menu():
+    database_uri = input(DATABASE_PROMPT)
+    if not database_uri:
+        load_dotenv()
+        database_uri = os.environ["DATABASE_URL"]
+    connex = psycopg2.connect(database_uri)
+    data.create_tables(connex)
+
+
+    while (user_choice := input(MENU_PROMPT)) != "6":
+        try:
+            MENU_OPTIONS[user_choice](connex)
+        except KeyError:
+            print("Choix inconnu !")
+
+menu()
--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-connex = psycopg2.connect()
+from selectors import EpollSelector
 
 CREATE_POLLS = '''CREATE TABLE IF NOT EXISTS polls (id SERIAL PRIMARY KEY, title TEXT, owner_username TEXT);'''
 CREATE_OPTIONS = '''CREATE TABLE IF NOT EXISTS options (id SERIAL PRIMARY KEY, option_text TEXT, poll_id INTEGER);'''
@@ -6,14 +6,58 @@ CREATE_VOTES = '''CREATE TABLE IF NOT EXISTS votes (username TEXT, option_id INT
 SELECT_ALL_POLLS = '''
 SELECT *
 FROM polls;'''
-SELECT_POLL_WITH_OPTION = '''SELECT polls.*
+
+SELECT_POLL_WITH_OPTION = '''
+SELECT *
 FROM polls
 JOIN options
 ON polls.id = options.poll_id
-WHERE polls.id = %s;
+WHERE polls.id = (
+    SELECT id
+    FROM polls
+    ORDER BY polls.id DESC
+    LIMIT 1);
 '''
+INSERT_POLL_RETURN_ID = "INSERT INTO polls (title, owner_username) VALUES (%s, %s) RETURNING id;"
 INSERT_OPTION = '''INSERT INTO options (option_text, poll_id) values %s;'''
 INSERT_VOTE = '''INSERT INTO votes (username, option_id) VALUES (%s, %s);'''
+OBTAIN_LAST_POLL = '''
+SELECT *
+FROM polls
+JOIN options
+ON polls.id = options.poll_id
+WHERE polls.id = (
+    SELECT id
+    FROM polls
+    ORDER BY polls DESC
+    LIMIT 1
+    );
+'''
+OBTAIN_LAST_POLL_V2 = '''
+WITH last_id AS (
+    SELECT id FROM polls ORDER BY id DESC LIMIT 1
+    )
+SELECT *
+FROM polls
+JOIN options
+ON polls.id = options.poll_id
+WHERE polls.id = (SELECT * FROM last_id);
+'''
+SELECT_RANDOM_VOTE = '''
+SELECT *
+FROM votes
+WHERE option_id = %s
+ORDER BY RANDOM()
+LIMIT 1;
+'''
+SELECT_POLL_VOTE_DETAILS = '''
+option_id, option_text, COUNT(votes.option_id) AS vote_count, COUNT(votes.option_id) * 100 / sum(count(votes.option_id)) AS pourcentage
+FROM options
+LEFT JOIN votes
+ON options.id = votes.option_id
+WHERE options.poll_id = %s
+GROUP BY options.option_id;
+'''
 
 def create_tables(connex):
     with connex:
@@ -42,17 +86,30 @@ def get_polls_details(connex, poll_id):
 def get_poll_and_vote_results(connex, poll_id):
     with connex:
         with connex.cursor() as cursor:
-            pass
+            cursor.execute(SELECT_POLL_VOTE_DETAILS, (poll_id,))
+            return cursor.fetchall()
 
 def get_random_poll_vote(connex, option_id):
     with connex:
         with connex.cursor() as cursor:
-            pass
+            cursor.execute(SELECT_RANDOM_VOTE, option_id)
+            return cursor.fetchone()
 
 def create_poll(connex, title, owner, options):
     with connex:
         with connex.cursor() as cursor:
-            pass
+            cursor.execute(INSERT_POLL_RETURN_ID, (title, owner))
+            poll_id = cursor.fetchone()[0]
+            options_values = [(option_text, poll_id) for option_text in options]
+            for option in options_values:
+                cursor.execute(INSERT_OPTION, option)
+
+                ## Il aurait été possible d'utiliser une fn de psycopg2 à la place de cette dernière boucle :
+                ## execute_values(cursor, INSERT_OPTION, option_values)
+                ## La fonction execute_values prend trois arguments :
+                # Le curseur qui exécutera les requêtes.
+                # La requête qui sera exécutée pour chaque valeur.
+                # Une liste de tup, où chaque tup correspond à ce qui est entré dans l’une des requêtes.
 
 def add_poll_vote(connex, username, option_id):
     with connex:
